156e0577cacb27ac4fa7e3ab8c41969290f2ec92
Akasoe <yudasakajr@gmail.com>
2023-07-05 19:15:48 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function submitdata() {
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("pesan").value;
+
+  alert("Sended!");
+}
